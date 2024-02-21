d2329977a2302cb2f704ac865b78c6a0bf541a4f
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 09:12:51 +0300

    Добавьте файлы проекта.

---
+++ normalization/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace normalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало диапазона:");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите конец диапазона:");
+            int end = int.Parse(Console.ReadLine());
+
+            // Нормализация границ диапазона
+            if (start > end)
+            {
+                int temp = start;
+                start = end;
+                end = temp;
+            }
+
+            Console.WriteLine($"Четные числа в диапазоне от {start} до {end}:");
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
